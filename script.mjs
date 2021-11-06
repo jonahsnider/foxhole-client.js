7d6b66bc3672826fc0db620f1aa71197c824656d
Jonah Snider <jonah@jonahsnider.com>
2021-11-06 10:59:24 -0700

    docs: remove example script

--- script.mjs
+++
@@ -1,9 +0,0 @@
-import Foxhole from './tsc_output/src/index.js';
-
-const foxhole = new Foxhole();
-
-const names = await foxhole.map.fetchMapNames();
-
-const data = await foxhole.map.fetchDynamicPublicData(names[0]);
-
-console.log(data);
