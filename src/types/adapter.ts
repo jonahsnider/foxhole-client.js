a1c77722f3e880fbf655f63ac4b22eafd9b7c049
Jonah Snider <jonah@jonahsnider.com>
2022-01-08 23:34:50 -0800

    refactor: remove unnecessary `@internal` annotations

--- src/types/adapter.ts
+++ src/types/adapter.ts
@@ -1,18 +1,9 @@
-/**
- * @internal
- */
 export type Adapter<A, B> = Deserializer<A, B> & Serializer<A, B>;
 
-/**
- * @internal
- */
 export interface Deserializer<A, B> {
 	deserialize(serialized: A): B;
 }
 
-/**
- * @internal
- */
 export interface Serializer<A, B> {
 	serialize(deserialized: B): A;
 }
